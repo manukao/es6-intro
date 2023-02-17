d79bdffa6f3c35f9ca0f113445eb28f84f55932d
Manuel Kao <manuelsko@live.de>
2023-02-17 07:00:19 +0100

    destructure student challenge

--- index.js
+++ index.js
@@ -32,3 +32,28 @@ const {
 // console.log(`${name} plays for ${club}`);
 
 console.log(`${name} lives in ${city}`);
+
+/*
+    **** Challenge ****
+    
+    For this challenge destruture the following object.
+*/
+
+const student = {
+  name: "Kyle",
+  age: 24,
+  projects: {
+    diceGame: "Two player dice game using JavaScript",
+  },
+};
+
+const {
+  name,
+  age,
+  projects: { diceGame },
+} = student;
+
+console.log(name);
+console.log(age);
+
+console.log(diceGame);
