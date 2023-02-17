9cfc379a2f5bd3eb69a4ee1b6321a03065749d09
Manuel Kao <manuelsko@live.de>
2023-02-17 06:49:08 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let word1 = "manu";
+let word2 = "kao";
+let num1 = 3;
+let num2 = 7;
+
+const someText = `${word1} ${num1 * num2} ${word2}`;
+console.log(someText);
+
+//let example = `${word1} ${word2}`;
+//console.log(example);
+//document.getElementById("example").innerText = example;
